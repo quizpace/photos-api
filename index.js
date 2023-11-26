503806dd3bf15dfee2a687a130446a68ccf495be
Avi <drazninavi@gmail.com>
2023-11-26 18:03:08 +0200

    a

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors"); // Import the cors package
 
 const app = express();
+
+// Use cors middleware to enable CORS for all routes
+app.use(cors());
 
 // Set up Multer storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'photos'); // Files will be saved in the 'photos' directory
+    cb(null, "photos"); // Files will be saved in the 'photos' directory
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}${path.extname(file.originalname)}`);
@@ -18,11 +22,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Route to handle file upload
-app.post('/upload', upload.single('photo'), (req, res) => {
+app.post("/upload", upload.single("photo"), (req, res) => {
   if (!req.file) {
-    return res.status(400).send('No file uploaded.');
+    return res.status(400).send("No file uploaded.");
   }
-  res.send('File uploaded successfully.');
+  res.send("File uploaded successfully.");
 });
 
 // Start the server
