bbe7915295d9e6be85c3e79b813ecc9cb7d66f66
Avi <drazninavi@gmail.com>
2023-11-26 19:00:31 +0200

    a

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
+// const express = require("express");
+// const multer = require("multer");
+// const path = require("path");
+// const cors = require("cors"); // Import the cors package
+
+// const app = express();
+
+// // Serve static files from the 'photos' directory
+// app.use("/photos", express.static(path.join(__dirname, "photos")));
+// // Use cors middleware to enable CORS for all routes
+// app.use(cors());
+
+// // Set up Multer storage configuration
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "photos"); // Files will be saved in the 'photos' directory
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+//   },
+// });
+
+// // Set up multer upload
+// const upload = multer({ storage });
+
+// // Route to handle file upload
+// app.post("/upload", upload.single("photo"), (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).send("No file uploaded.");
+//   }
+//   res.send("File uploaded successfully.");
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello, this is your server!"); // Or any other response you want for the root URL
+// });
+
+// // Start the server
+// const PORT = 6800;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const cors = require("cors"); // Import the cors package
+const cors = require("cors");
 
 const app = express();
-
-// Serve static files from the 'photos' directory
-app.use("/photos", express.static(path.join(__dirname, "photos")));
-// Use cors middleware to enable CORS for all routes
 app.use(cors());
 
-// Set up Multer storage configuration
+// Serve uploaded photos from the 'photos' directory
+app.use("/uploads", express.static(path.join(__dirname, "photos")));
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "photos"); // Files will be saved in the 'photos' directory
+    cb(null, "photos");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}${path.extname(file.originalname)}`);
   },
 });
 
-// Set up multer upload
 const upload = multer({ storage });
 
-// Route to handle file upload
+// Route to handle photo upload
 app.post("/upload", upload.single("photo"), (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
@@ -31,11 +71,11 @@ app.post("/upload", upload.single("photo"), (req, res) => {
   res.send("File uploaded successfully.");
 });
 
+// Root endpoint
 app.get("/", (req, res) => {
-  res.send("Hello, this is your server!"); // Or any other response you want for the root URL
+  res.send("Hello, this is your server!");
 });
 
-// Start the server
 const PORT = 6800;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
